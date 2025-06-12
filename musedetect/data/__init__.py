26683047441a81f5f9dbd8ebd98aa8db35259d7f
Seon82 <46298009+Seon82@users.noreply.github.com>
2025-06-12 15:11:03 +0200

    feat: add example notebook

--- musedetect/data/__init__.py
+++ musedetect/data/__init__.py
@@ -1,10 +1,6 @@
-__all__ = [
-    "train_test_split",
-    "MedleyDBPreprocessor",
-    "MedleyDBDataset",
-]
+__all__ = ["train_test_split", "MedleyDBPreprocessor", "MedleyDBDataset", "get_all_instruments"]
 
 
-from .medleydb import MedleyDBDataset
+from .medleydb import MedleyDBDataset, get_all_instruments
 from .preprocess import MedleyDBPreprocessor
 from .utils import train_test_split
